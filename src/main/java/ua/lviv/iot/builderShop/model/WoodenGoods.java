08bb36b8a57e65f3ed31694e510da400387c1c78
david8801 <44001477+david8801@users.noreply.github.com>
2019-06-08 07:32:22 -0700

    JavaLab4

---
+++ src/main/java/ua/lviv/iot/builderShop/model/WoodenGoods.java
@@ -0,0 +1,41 @@
+package ua.lviv.iot.builderShop.model;
+
+public class WoodenGoods extends BuilderGoods {
+
+
+    public WoodenGoods() {
+    }
+
+    public WoodenGoods(String name, double weight, String material, int height, TypeOfGoods typeOfGoods, int price,
+                       String producer, int width, Resist waterResist, int length) {
+        super(name, weight, material, height, typeOfGoods, price, producer, width);
+        this.waterResist = waterResist;
+        this.length = length;
+    }
+
+    private Resist waterResist;
+    private int length;
+
+
+    public Resist getWaterResist() {
+        return waterResist;
+    }
+
+    public void setWaterResist(Resist waterResist) {
+        this.waterResist = waterResist;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "\n" + super.toString() + "waterResist = " + waterResist + ", length = " + length;
+    }
+
+}
